4573145154cb07420c1d40647d2e3e904652a1b2
samuelvieira-dev <augusto.samuel@yahoo.com.br>
2024-11-17 19:23:34 -0300

    preparando ambiente

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "RH MANGNT";
+Route::get('/email', function () {
+    
+    Mail::raw('Mensagem de teste de RH MANGNT', function(Message $message){
+        $message->to('teste@gmail.com')
+        ->subject('Bem-vindo ao RH MANGNT')
+        ->from('rh@rhmangnt.com');
+    });
+
+    echo 'Email enviado com sucesso.';
 });
