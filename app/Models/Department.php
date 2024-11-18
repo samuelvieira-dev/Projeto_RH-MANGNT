dab793e2cac7a859aaf5e63932f9027b72ba6b9c
samuelvieira-dev <augusto.samuel@yahoo.com.br>
2024-11-18 20:43:18 -0300

    Configurando relações no Eloquent entre as tabelas

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+    use HasFactory;
+    public function users()
+    {
+        // each department can be filled with multiple users
+        return $this->belongsToMany(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    use HasFactory;
+
+    public function detail()
+    {
+        //each user has one user_details
+        return $this->hasOne(UserDetail::class);
+    }
+
+    public function department()
+    {
+        //this user belongs to a department
+        return $this->belongsTo(Department::class);
+    }
 }
