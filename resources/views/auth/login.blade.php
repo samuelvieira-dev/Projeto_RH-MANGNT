080ec953b1897c0a346f6fb660d8578481846cd7
samuelvieira-dev <augusto.samuel@yahoo.com.br>
2024-11-22 19:58:38 -0300

    Criação e ajustes no sistema de formulário

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,46 @@
 <x-layout-guest page-title='Login'>
-    <p class="display-6 text-center my-5">FORMULÁRIO DE LOGIN</p>
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-4">
+    
+                <!-- logo -->
+                <div class="text-center mb-5">
+                    <img src="{{ asset('assets/images/logo.png') }}" alt="Logo" width="200px">
+                </div>
+    
+                <!-- login form -->
+                <div class="card p-5">
+    
+                    <form action="{{ route('login') }}" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email">
+                            @error("email")
+                                <div class='text-bg-danger'>{{ $message }}</div>   
+                            @enderror
+                        </div>
+    
+                        <div class="mb-3">
+                            <label for="password">Senha</label>
+                            <input type="password" class="form-control" id="password" name="password">
+                            @error("password")
+                                <div class='text-bg-danger'>{{ $message }}</div>   
+                            @enderror
+                        </div>
+    
+                        <div class="d-flex justify-content-between align-items-center">
+                            <a href="#">Esqueceu a sua senha?</a>
+                            <button type="submit" class="btn btn-primary px-4">Entrar</button>
+                        </div>
+    
+                    </form>
+    
+                </div>
+    
+            </div>
+        </div>
+    </div>
+
 </x-layout-guest>
